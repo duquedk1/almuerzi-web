a4f3ed29d988aef5307f36307159285417861f7c
Daniel Duque <duquedk1@gmail.com>
2021-12-12 22:00:38 -0500

    fix main.js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+const stringToHTML = (s) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(s, "text/html");
+  return doc.body.firstChild;
+};
+
+const renderItem = (item) => {
+  const element = stringToHTML(`<li data-id="${item._id}">${item.name}</li>`);
+  element.addEventListener("click", () => {
+    element.classList.add("selected");
+    element.classList.remove("selected")
+  });
+  return element;
+};
+
 window.onload = () => {
   fetch("https://serverless-duquedk1.vercel.app/api/meals")
     .then((res) => res.json())
     .then((data) => {
       const mealsList = document.getElementById("meals-list");
-      const template = data.map((x) => "<li>" + x.name + "</li>").join("");
-      mealsList.innerHTML = template;
+      const submit = document.getElementById("submit");
+      const listItems = data.map(renderItem);
+      mealsList.removeChild(mealsList.firstElementChild);
+      //const template = data.map(renderItem).join("");
+      listItems.forEach((element) => mealsList.appendChild(element));
+      //mealsList.innerHTML = template;
+      submit.removeAttribute("disabled");
     });
 };
